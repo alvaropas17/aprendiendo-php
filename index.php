cfa445b430775fc0fecff203f79d081c4eef1186
alvaropas17 <alvpastor17@gmail.com>
2025-09-06 17:48:51 +0200

    aprendiendo tipado dinámico

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
 
 // Hay que hacer un ";" por cada instrucción
+// echo "Hola, PHP";
+
+/* Este es un comentario */
+
+
+/*
 echo "Hola, PHP";
+echo "Hola, PHP";
+echo "Hola, PHP";
+echo "Hola, PHP";
+echo "Hola, PHP";
+*/
+
+// Variables
+
+$my_string = "Esto es una cadena de texto";
+
+// Para hacer un salto de línea
+echo "Hola, PHP \n";
+echo $my_string . "\n";
+echo gettype($my_string) . "\n";
+
+// Tipado dinámico
+$my_string = 6;
+echo $my_string . " \n";
+echo  gettype($my_string) . "\n";
+
 
