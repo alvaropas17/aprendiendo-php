3f87f3734a323fa89b15227e91917fd90cfa1cd4
alvaropas17 <alvpastor17@gmail.com>
2025-09-24 13:15:21 +0200

    ejercicio concesionario empezado

--- ejercicio3Concesionario/ejConcesionario.php
+++ ejercicio3Concesionario/ejConcesionario.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ej Concesionario</title>
 </head>
+
 <body>
     <h2>Ejercicio 3</h2>
     <h3>Un concesionario de venta de coches de segunda mano que vende coches de distintas marcas, nos pide que hagamos una aplicación web que le muestre el número de coches en stock y vendidos de cada marca.
-Usando PHP, debemos crear un array que contenga la marca y tanto el número de coches vendidos como en stock para cada marca del concesionario y mostrar una página web que muestre una lista con los datos que nos pide el cliente.</h3>
+        Usando PHP, debemos crear un array que contenga la marca y tanto el número de coches vendidos como en stock para cada marca del concesionario y mostrar una página web que muestre una lista con los datos que nos pide el cliente.</h3>
 
 
-<?php
+    <?php
 
+    $concesionario = [
+        "2" => "Tesla",
+        "5" => "Mercedes"
+    ];
 
-
-?>
+    ?>
 
 
 
 
 </body>
+
 </html>
